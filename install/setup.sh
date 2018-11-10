e2333530bf298024d396086e0a174c1128d3e599
Matt Duftler <duftler@google.com>
2018-11-10 10:45:06 -0500

    WIP

--- install/setup.sh
+++ install/setup.sh
@@ -38,12 +38,16 @@ gcloud projects add-iam-policy-binding $PROJECT_ID \
   --member serviceAccount:$SA_EMAIL \
   --role roles/owner
 
-bold "Creating bucket $BUCKET_URI..."
-# bold "Using existing bucket $BUCKET_URI..."
+# TODO: Could verify ACLs here. In the meantime, error messages should suffice.
+gsutil ls $BUCKET_URI
 
-gsutil mb -p $PROJECT_ID $BUCKET_URI
+if [ $? != 0 ]; then
+  bold "Creating bucket $BUCKET_URI..."
 
-
+  gsutil mb -p $PROJECT_ID $BUCKET_URI
+else
+  bold "Using existing bucket $BUCKET_URI..."
+fi
 
 #gcloud beta container --project $PROJECT_ID clusters create $CLUSTER_NAME --zone $ZONE --username "admin" --cluster-version "1.11.2" --machine-type "n1-highmem-4" --image-type "COS" --disk-type "pd-standard" --disk-size "100" --service-account $SA_EMAIL --num-nodes "3" --enable-stackdriver-kubernetes --addons HorizontalPodAutoscaling,HttpLoadBalancing --enable-autoupgrade --enable-autorepair
 
