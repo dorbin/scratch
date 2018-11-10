659e00a5fd272a67b29265655886bd989be69dc1
Matt Duftler <duftler@google.com>
2018-11-10 08:42:30 -0500

    Create setup_properties.sh

---
+++ install/setup_properties.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+if [ -z "$PROJECT_ID" ]; then
+  echo 'Project id not set; setting it...'
+  PROJECT_ID=$(gcloud info --format='value(config.project)')
+fi
+
+cat >properties <<EOL
+PROJECT_ID=$PROJECT_ID
+GKE_CLUSTER=spin-demo
+ZONE=us-west1-b
+EOL
+
+#if [ -f bucket.txt ]; then
+#  export BUCKET_NAME=$(cat bucket.txt)
+#else
+#  export BUCKET_NAME="spin-gcs-bucket-$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 20 | head -n 1)-$(date +"%s")"
+#  echo $BUCKET_NAME > bucket.txt
+#fi
+
+#export BUCKET_URI="gs://$BUCKET_NAME"
+
+#if [ -f account.txt ]; then
+#  SERVICE_ACCOUNT_NAME=$(cat account.txt)
+#else
+#  SERVICE_ACCOUNT_NAME="spin-acc-$(date +"%s")"
+#  echo $SERVICE_ACCOUNT_NAME > account.txt
+#fi
+
+#export GCS_TOPIC="spin-gcs-topic"
+#export GCS_SUB="spin-gcs-sub"
+#export GCR_TOPIC="projects/${PROJECT_ID}/topics/gcr"
+#export GCR_SUB="spin-gcr-sub"
+
+#export SPIN_GCS_ACCOUNT="my-gcs-account"
+
+#export SPIN_GCS_PUB_SUB="my-gcs-pub-sub"
+#export SPIN_GCR_PUB_SUB="my-gcr-pub-sub"
+
+#export SPINNAKER_VERSION='$(hal version latest --daemon-endpoint http://spin-halyard.spinnaker:8064 -q)'
