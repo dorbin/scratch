7b5fecc9c26088938e4433de3ba852a43509b146
Matthew Duftler <duftler@google.com>
2019-01-30 12:06:06 -0500

    Install spin.

---
+++ install_spin.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+curl -LO https://storage.googleapis.com/spinnaker-artifacts/spin/$(curl -s https://storage.googleapis.com/spinnaker-artifacts/spin/latest)/linux/amd64/spin
+
+chmod +x spin
+mv spin ~
+
+grep -q '^alias spin=~/spin' ~/.bashrc || echo 'alias spin=~/spin' >> ~/.bashrc
