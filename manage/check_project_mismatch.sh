efe88185565cf239f25f6edb60e1123c35d8e139
Matthew Duftler <duftler@google.com>
2019-05-08 13:25:48 -0400

    Correct gcloud default/config project id mismatch instead of just warning.

--- manage/check_project_mismatch.sh
+++ manage/check_project_mismatch.sh
@@ -7,8 +7,11 @@ bold() {
 source ~/scratch/install/properties
 
 GCLOUD_PROJECT_ID=$(gcloud info --format='value(config.project)')
+GCLOUD_PROJECT_ID=${GCLOUD_PROJECT_ID:-'not set'}
+
+gcloud config set project $PROJECT_ID
 
 if [ "$GCLOUD_PROJECT_ID" != $PROJECT_ID ]; then
-  bold "Warn: Your Spinnaker config references GCP project id $PROJECT_ID, but your gcloud default project id is $GCLOUD_PROJECT_ID."
-  bold "For safety when executing gcloud commands, you should use 'gcloud config set project $PROJECT_ID' to change the gcloud default."
+  bold "Your Spinnaker config references GCP project id $PROJECT_ID, but your gcloud default project id was $GCLOUD_PROJECT_ID."
+  bold "For safety when executing gcloud commands, 'gcloud config set project $PROJECT_ID' has been used to change the gcloud default."
 fi
