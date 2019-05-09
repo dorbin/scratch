d00e767ad67eb384548bb2bfe96a87a8e4e2c9db
Matthew Duftler <duftler@google.com>
2019-05-09 07:20:38 -0400

    Put gcloud call within the conditional where it belongs.

--- manage/check_project_mismatch.sh
+++ manage/check_project_mismatch.sh
@@ -9,9 +9,9 @@ source ~/scratch/install/properties
 GCLOUD_PROJECT_ID=$(gcloud info --format='value(config.project)')
 GCLOUD_PROJECT_ID=${GCLOUD_PROJECT_ID:-'not set'}
 
-gcloud config set project $PROJECT_ID
-
 if [ "$GCLOUD_PROJECT_ID" != $PROJECT_ID ]; then
+  gcloud config set project $PROJECT_ID
+
   bold "Your Spinnaker config references GCP project id $PROJECT_ID, but your gcloud default project id was $GCLOUD_PROJECT_ID."
   bold "For safety when executing gcloud commands, 'gcloud config set project $PROJECT_ID' has been used to change the gcloud default."
 fi
